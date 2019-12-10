842d814680a2e6dad75a206b903bd0fc98849523
TheWillGabriel <will.gabriel@gmail.com>
2019-12-10 09:45:26 -0700

    Refactor improve efficiency of checks

--- 04/day_04.rb
+++ 04/day_04.rb
@@ -1,30 +1,30 @@
 require 'benchmark'
 
 min_max = File.read('input.txt').split('-')
-range = min_max[0].to_i..min_max[1].to_i
+range = (min_max[0].to_i)..(min_max[1].to_i)
 
 def find_passwords(range)
   passwords = []
 
   range.each do |number|
-    digits = number.digits.reverse
-    next unless adjacent_match?(digits)
-    next unless increase_only?(digits)
+    next unless increase_only?(number)
+    next unless adjacent_match?(number)
 
     passwords << number
   end
   passwords
 end
 
-def adjacent_match?(digits)
-  digits.each_index do |index|
-    break if index == digits.size - 1
-    return true if digits[index] == digits[index + 1]
+def adjacent_match?(number)
+  groups = number.to_s.scan(/((\d)\2*)/).map(&:first)
+  groups.each do |group|
+    return true if group.length == 2
   end
   false
 end
 
-def increase_only?(digits)
+def increase_only?(number)
+  digits = number.digits.reverse
   digits.each_index do |index|
     break if index == digits.size - 1
     return false if digits[index] > digits[index + 1]
