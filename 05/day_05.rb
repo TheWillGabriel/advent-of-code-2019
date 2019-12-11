d85dc8721234ce5ba857994f41721db40d9cf9ea
TheWillGabriel <will.gabriel@gmail.com>
2019-12-11 10:02:18 -0700

    Fix bug in argument assignment

--- 05/day_05.rb
+++ 05/day_05.rb
@@ -7,22 +7,22 @@ def run_intcode(memory)
     mode1 = intcode[opcode_address] / 100 % 10
     mode2 = intcode[opcode_address] / 1000 % 10
     mode3 = intcode[opcode_address] / 10_000 % 10
-    argument1 = opcode_address + 1
-    argument2 = opcode_address + 2
-    argument3 = opcode_address + 3
+    argument1 = intcode[opcode_address + 1]
+    argument2 = intcode[opcode_address + 2]
+    argument3 = intcode[opcode_address + 3]
 
     if opcode == 1
-      intcode = add(intcode: intcode,
-                    modes: [mode1, mode2],
-                    arguments: [argument1, argument2, argument3])
+      add(intcode: intcode,
+          modes: [mode1, mode2],
+          arguments: [argument1, argument2, argument3])
       opcode_address += 4
     elsif opcode == 2
-      intcode = multiply(intcode: intcode,
-                         modes: [mode1, mode2],
-                         arguments: [argument1, argument2, argument3])
+      multiply(intcode: intcode,
+               modes: [mode1, mode2],
+               arguments: [argument1, argument2, argument3])
       opcode_address += 4
     elsif opcode == 3
-      intcode[address1] = fetch_input
+      intcode[argument1] = fetch_input
       opcode_address += 2
     elsif opcode == 4
       send_output(intcode: intcode,
@@ -30,10 +30,10 @@ def run_intcode(memory)
                   argument: argument1)
       opcode_address += 2
     end
-    end
+  end
 
   intcode
-  end
+end
 
 # Sum parameter will always be in position mode
 def add(intcode:, modes:, arguments:)
@@ -41,8 +41,6 @@ def add(intcode:, modes:, arguments:)
   addend2 = modes[1] == 1 ? arguments[1] : intcode[arguments[1]]
   sum_index = arguments[2]
   intcode[sum_index] = addend1 + addend2
-
-  intcode
 end
 
 # Product parameter will always be in position mode
@@ -51,13 +49,11 @@ def multiply(intcode:, modes:, arguments:)
   addend2 = modes[1] == 1 ? arguments[1] : intcode[arguments[1]]
   product_index = arguments[2]
   intcode[product_index] = addend1 * addend2
-
-  intcode
 end
 
 def fetch_input
   puts 'Enter the ID of the system to test'
-  gets.chomp
+  gets.chomp.to_i
 end
 
 def send_output(intcode:, mode:, argument:)
@@ -87,6 +83,4 @@ end
 
 memory = File.read('input.txt').split(',').map(&:to_i)
 
-input = find_input(memory, 19_690_720)
-
-puts input
+run_intcode(memory)
