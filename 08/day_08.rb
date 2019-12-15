0bfdbb192b8ad0f79ed4b9629ebbde626cd06d33
TheWillGabriel <will.gabriel@gmail.com>
2019-12-15 15:23:08 -0700

    Render black and white image

--- 08/day_08.rb
+++ 08/day_08.rb
@@ -1,3 +1,8 @@
+require 'colorize'
+
+WHITE = '  '.colorize(background: :white)
+BLACK = '  '.colorize(background: :black)
+
 # Images encoded in layers of width * height digit length
 class Image
   def initialize(image_data, width, height)
@@ -13,7 +18,39 @@ class Image
     verification_layer.count('1') * verification_layer.count('2')
   end
 
+  def render
+    rows = flatten.scan(/.{#{@width}}/)
+    rows.each_index do |index|
+      rows[index] = rows[index].gsub('0', BLACK).gsub('1', WHITE)
+    end
+    rows
+  end
+
+  def flatten
+    image = []
+
+    @layers.reverse.each do |layer|
+      image = flatten_layer(image, layer)
+    end
+
+    image.join
+  end
+
   private
+
+    def flatten_layer(image, layer)
+      output = image
+
+      layer.each_char.with_index do |pixel, index|
+        if pixel == '0'
+          output[index] = '0'
+        elsif pixel == '1'
+          output[index] = '1'
+        end
+      end
+
+      output
+    end
 
     def least_zeroes
       @layers.index(@layers.min { |a, b| a.count('0') <=> b.count('0') })
@@ -31,4 +68,4 @@ end
 
 input = File.read('input.txt')
 image = Image.new(input, 25, 6)
-puts image.verify
+puts image.render
