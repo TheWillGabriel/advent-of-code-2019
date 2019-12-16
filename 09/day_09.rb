8fd297016d9ce1dfd2f3a0e02169abce40335e75
TheWillGabriel <will.gabriel@gmail.com>
2019-12-16 10:48:29 -0700

    Implement relative_base

--- 09/day_09.rb
+++ 09/day_09.rb
@@ -1,15 +1,18 @@
 require 'pry'
 
-# Output modes: :console, :return
+# Output modes: :list, :single
+# list: Stores as an array
+# single: Stores a single result then pauses
 class Computer
   attr_reader :done
 
-  def initialize(intcode, output_mode = :console)
+  def initialize(intcode, output_mode = :list)
     @memory = intcode.dup
     @inputs = nil
     @pointer = 0
+    @relative_base = 0
     @output_mode = output_mode
-    @output = nil
+    @output = []
     @done = false
     @paused = false
   end
@@ -26,7 +29,7 @@ class Computer
       @done = true if @memory[@pointer] == 99 || @pointer + 1 >= @memory.length
     end
 
-    terminate
+    @output
   end
 
   private
@@ -40,7 +43,7 @@ class Computer
       when 2
         multiply(**instruction_params)
       when 3
-        fetch_input(mode: parameter_modes[0], argument: @pointer + 1)
+        fetch_input(**instruction_params)
       when 4
         send_output(**instruction_params)
       when 5
@@ -51,15 +54,8 @@ class Computer
         less_than(**instruction_params)
       when 8
         equals(**instruction_params)
-      end
-    end
-
-    # Determines what to return when run() is finished
-    def terminate
-      if @output_mode == :console
-        @memory
-      elsif @output_mode == :return
-        @output
+      when 9
+        relative_base(**instruction_params)
       end
     end
 
@@ -81,42 +77,53 @@ class Computer
     # mode 0: Position
     # mode 1: Immediate
     # mode 2: Relative
-    def value(address:, mode:)
+    # nil addresses return zero
+    def read(address:, mode:)
       case mode
       when 0
-        @memory[address]
+        @memory[address] || 0
       when 1
         address
       when 2
-        @memory[@pointer + address]
+        @memory[@relative_base + address] || 0
       else
         puts "Warning: invalid mode given for address #{address}"
       end
     end
 
-    # Sum parameter will always be in position mode
+    # mode 0: Position
+    # mode 2: Relative
+    # writes are never in immediate mode
+    def write(address:, mode:)
+      case mode
+      when 0
+        address
+      when 2
+        @relative_base + address
+      end
+    end
+
     def add(modes:, arguments:)
-      addend1 = value(address: arguments[0], mode: modes[0])
-      addend2 = value(address: arguments[1], mode: modes[1])
-      sum_address = arguments[2]
+      addend1 = read(address: arguments[0], mode: modes[0])
+      addend2 = read(address: arguments[1], mode: modes[1])
+      sum_address = write(address: arguments[2], mode: modes[2])
 
       @memory[sum_address] = addend1 + addend2
       @pointer += 4
     end
 
-    # Product parameter will always be in position mode
     def multiply(modes:, arguments:)
-      factor1 = value(address: arguments[0], mode: modes[0])
-      factor2 = value(address: arguments[1], mode: modes[1])
-      product_address = arguments[2]
+      factor1 = read(address: arguments[0], mode: modes[0])
+      factor2 = read(address: arguments[1], mode: modes[1])
+      product_address = write(address: arguments[2], mode: modes[2])
 
       @memory[product_address] = factor1 * factor2
       @pointer += 4
     end
 
     # Pulls next input from the front of input array if any exist
-    def fetch_input(mode:, argument:)
-      destination_address = value(address: argument, mode: mode)
+    def fetch_input(modes:, arguments:)
+      destination_address = write(address: arguments[0], mode: modes[0])
       if @inputs.empty?
         puts 'Enter the ID of the system to test'
         input = gets.chomp.to_i
@@ -128,37 +135,37 @@ class Computer
     end
 
     def send_output(modes:, arguments:)
-      value = value(address: arguments[0], mode: modes[0])
-      if @output_mode == :console
-        puts "Diagnostic code: #{value}"
-      elsif @output_mode == :return
+      value = read(address: arguments[0], mode: modes[0])
+      if @output_mode == :list
+        @output << value
+      elsif @output_mode == :single
         @output = value
       end
       @pointer += 2
-      @paused = true if @output_mode == :return
+      @paused = true if @output_mode == :single
     end
 
     # Returns the new pointer index
     def jump_if_true(modes:, arguments:)
-      value = value(address: arguments[0], mode: modes[0])
-      address = value(address: arguments[1], mode: modes[1])
+      value = read(address: arguments[0], mode: modes[0])
+      address = read(address: arguments[1], mode: modes[1])
       jump = !value.zero?
       @pointer = jump ? address : @pointer + 3
     end
 
     # Returns the new pointer index
     def jump_if_false(modes:, arguments:)
-      value = value(address: arguments[0], mode: modes[0])
-      address = value(address: arguments[1], mode: modes[1])
+      value = read(address: arguments[0], mode: modes[0])
+      address = read(address: arguments[1], mode: modes[1])
       jump = value.zero?
       @pointer = jump ? address : @pointer + 3
     end
 
     # Result will always be in position mode
     def less_than(modes:, arguments:)
-      term1 = value(address: arguments[0], mode: modes[0])
-      term2 = value(address: arguments[1], mode: modes[1])
-      result_address = arguments[2]
+      term1 = read(address: arguments[0], mode: modes[0])
+      term2 = read(address: arguments[1], mode: modes[1])
+      result_address = write(address: arguments[2], mode: modes[2])
 
       @memory[result_address] = term1 < term2 ? 1 : 0
       @pointer += 4
@@ -166,16 +173,21 @@ class Computer
 
     # Result will always be in position mode
     def equals(modes:, arguments:)
-      term1 = value(address: arguments[0], mode: modes[0])
-      term2 = value(address: arguments[1], mode: modes[1])
-      result_address = arguments[2]
+      term1 = read(address: arguments[0], mode: modes[0])
+      term2 = read(address: arguments[1], mode: modes[1])
+      result_address = write(address: arguments[2], mode: modes[2])
 
       @memory[result_address] = term1 == term2 ? 1 : 0
       @pointer += 4
     end
+
+    def relative_base(modes:, arguments:)
+      @relative_base += read(address: arguments[0], mode: modes[0])
+      @pointer += 2
+    end
 end
 
-memory = File.read('example.txt').split(',').map(&:to_i)
+memory = File.read('input.txt').split(',').map(&:to_i)
 
 computer = Computer.new(memory)
-computer.run
+puts computer.run(1)
